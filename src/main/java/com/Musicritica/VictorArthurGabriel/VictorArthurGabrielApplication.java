0d6a9534b8a054eefa2c86fb8d7faa8e87d763d1
Victor Saccucci <victorsaccucciv@gmail.com>
2024-01-09 12:36:53 -0300

    Initial commit

---
+++ src/main/java/com/Musicritica/VictorArthurGabriel/VictorArthurGabrielApplication.java
@@ -0,0 +1,13 @@
+package com.Musicritica.VictorArthurGabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorArthurGabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorArthurGabrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Musicritica/VictorArthurGabriel/VictorArthurGabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musicritica.VictorArthurGabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorArthurGabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
