6589b9b0dce3b03e355e04615de997e30951f29d
ArthurSauerr <germanooarthuur@gmail.com>
2024-01-13 20:36:07 -0300

    Exception e entidade usuario com alguns metodos

---
+++ src/main/java/com/Musicritica/VictorArthurGabriel/exception/MusicriticaException.java
@@ -0,0 +1,5 @@
+package com.Musicritica.VictorArthurGabriel.exception;
+
+public class MusicriticaException extends Exception{
+    public MusicriticaException(String mensagem) { super(mensagem); }
+}
