41de0bcd3ddad21180d5d9c6d9d73e7b22eb0fc0
garciaa43 <gabrielcordeir.gg@gmailc.om>
2024-07-03 09:58:40 -0300

    feat: verificação da denuncia

--- src/main/java/com/Musicritica/VictorArthurGabriel/service/DenunciaService.java
+++ src/main/java/com/Musicritica/VictorArthurGabriel/service/DenunciaService.java
@@ -1,8 +1,12 @@
 package com.Musicritica.VictorArthurGabriel.service;
 
+import com.Musicritica.VictorArthurGabriel.entity.Comentario;
 import com.Musicritica.VictorArthurGabriel.entity.Denuncia;
 import com.Musicritica.VictorArthurGabriel.entity.Musica;
+import com.Musicritica.VictorArthurGabriel.entity.usuario.Usuario;
+import com.Musicritica.VictorArthurGabriel.repository.ComentarioRepository;
 import com.Musicritica.VictorArthurGabriel.repository.DenunciaRepository;
+import com.Musicritica.VictorArthurGabriel.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +18,25 @@ public class DenunciaService {
     @Autowired
     private DenunciaRepository repository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ComentarioRepository comentarioRepository;
+
     public Denuncia salvar(Denuncia denuncia) {
+        Usuario usuarioAutenticado = usuarioRepository.buscarPeloId(denuncia.getUsuario().getId());
+        Comentario comentarioEncontrado = comentarioRepository.buscarComentarioPorId(denuncia.getComentario().getId());
+
+        boolean denunciaExiste = repository.BuscarDenunciaPorUsuario(comentarioEncontrado.getId(),usuarioAutenticado.getId());
+
+        if (denunciaExiste) {
+            throw new IllegalArgumentException("Usuário já enviou uma denúncia para este comentário.");
+        }
+
         return repository.save(denuncia);
     }
+
 
     public List<Denuncia> listarTodos() {
         return repository.listarTodos();
@@ -36,6 +56,10 @@ public class DenunciaService {
     public boolean fecharDenuncia(Long id) {
         int rowsUpdated = repository.fecharDenunciaById(id);
         return rowsUpdated > 0;
+    }
+
+    public boolean verificarDenunciaExistente(Long comentarioId, Long usuarioId) {
+        return repository.BuscarDenunciaPorUsuario(comentarioId, usuarioId);
     }
 
 
